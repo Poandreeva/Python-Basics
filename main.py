8a039db02f843a56f24868e01a2c34abe948838d
Polina <polliand@yandex.ru>
2022-03-14 14:45:17 +0300

    Урок 4

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+import math
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(math.sin(90))   # вызов функции синус из модуля
+time.sleep(2)
