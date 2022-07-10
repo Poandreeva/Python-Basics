d81ff9eb593fdebd788fdd03414e67214430b70b
Polina <polliand@yandex.ru>
2022-07-10 14:07:01 +0300

    Урок 5

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+f0 = open('1.txt', 'r', encoding='utf-8')   #открыли, прочитали, и сделали так, чтобы кириллица читалась корректно
+f0.close()   #обязательно закрыть файл после
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#чтение из файла
+f = open(r'test.txt', 'r', encoding='utf-8')    # r для того чтобы путь правильно воспринималься или прописывать путь как надо
+for i in f:
+    print(i)  #двойной перенос строки из-за записи с новой строки в файле
+f.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+f = open(r'test.txt', 'r', encoding='utf-8')    # r для того чтобы путь правильно воспринималься или прописывать путь как надо
+for i in f:
+    print(i, end='')  #избежать доп переноса строки
+f.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#чтение файла и печать на экране без доп пробела
+f = open(r'test.txt', 'r', encoding='utf-8')
+test = f.read(10)  #первые 10 символов (включая перенос строк), фунция считает кол-во символов
+print(test)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+test = f.readline()  #считывает до переноса строки
+print(test)
+
+text = f.readlines(1)  #количество символов, если зацепляем следующую строку, то она считывается полностью
+print(text)
+f.close()
+# итог 4 способа считывания строк
+
+
+
+#запись в файл
+f = open(r'new.txt', 'w', encoding='utf-8')   #удаляет всю информацию которая была, поэтому стоит создать новый файл
+print('booyy\nlllllll\n', file=f)
+f.close()
+
+f = open(r'new.txt', 'w', encoding='utf-8')
+ar1 = 593820  #строка обязатлеьно
+f.write(f'123453\n{ar1}')  #тогда делаем f строку
+f.close()
+
+f = open(r'new.txt', 'w', encoding='utf-8')
+f.writelines(['gggiiiirl\nxoxo\ngossip\n'])  #передаем список из строк иначе пиздец
+f.close()
+
+# 3 способа записи в файл
+
+#менеджер контекста - сам закрывает файл - молодец
+# в теле области файл открыт, при выходе файл закрывается
+with open(r'new.txt', 'w', encoding='utf-8') as f:
+    f.writelines(['gggiiiirl\nxoxo\n'])
+
+with open(r'new2.txt', 'x', encoding='utf-8') as f:  # x создает и записывает в новый файл, со старым не работает
+    f.writelines(['bye\nbitches\n'])
+
+with open(r'new2.txt', 'а', encoding='utf-8') as f:   #a  всегда записывает далее без самостоятельного переноса
+    f.writelines(['345\n', '6543\n'])
